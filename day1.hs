9a0d5a8f0637c5a21aac6eb904a9b5e499e99671
Benjamin Meijs <benjamin.meijs@gmail.com>
2021-12-08 19:59:19 +0100

    add day 1

---
+++ day1.hs
@@ -0,0 +1,12 @@
+main = do
+    inputText <- readFile "inputs/day1.txt" 
+    let input = map read . lines $ inputText
+    print $ solveB input
+    
+solveA :: [Int] -> Int
+solveA xs = length .  filter (\(a, b) -> b > a)
+            $ zip xs (tail xs)
+
+solveB :: [Int] -> Int
+solveB xs = solveA . map (\(a, b, c) -> a + b + c) 
+            $ zip3 xs (tail xs) (tail $ tail xs)
